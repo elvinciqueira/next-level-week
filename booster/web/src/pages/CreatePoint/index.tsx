0d22cbdef2fcfec79928ac99fbadfedf1a53247a
elvinciqueira <elvinciqueira@gmail.com>
2020-06-05 15:38:15 -0300

    feat: upload de imagem

--- booster/web/src/pages/CreatePoint/index.tsx
+++ booster/web/src/pages/CreatePoint/index.tsx
@@ -12,6 +12,7 @@ import { toast } from 'react-toastify';
 
 import api from '../../services/api';
 import getValidationErrors from '../../utils/getValidationErrors';
+import DropZone from '../../components/DropZone';
 
 import './styles.css';
 
@@ -41,6 +42,7 @@ const CreatePoint: React.FC = () => {
   const [selectedCity, setSelectedCity] = useState('0');
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [selectedFile, setSelectedFile] = useState<File>();
   const history = useHistory();
 
   useEffect(() => {
@@ -117,16 +119,20 @@ const CreatePoint: React.FC = () => {
       const [latitude, longitude] = selectedPosition;
       const items = selectedItems;
 
-      const data = {
-        name,
-        email,
-        whatsapp,
-        uf,
-        city,
-        latitude,
-        longitude,
-        items,
-      };
+      const data = new FormData();
+
+      data.append('name', name);
+      data.append('email', email);
+      data.append('whatsapp', whatsapp);
+      data.append('uf', uf);
+      data.append('city', city);
+      data.append('latitude', String(latitude));
+      data.append('longitude', String(longitude));
+      data.append('items', items.join(','));
+
+      if (selectedFile) {
+        data.append('image', selectedFile);
+      }
 
       await api.post('points', data);
 
@@ -142,7 +148,7 @@ const CreatePoint: React.FC = () => {
         return;
       }
     }
-  }, [history, selectedCity, selectedItems, selectedPosition, selectedUf]);
+  }, [history, selectedCity, selectedFile, selectedItems, selectedPosition, selectedUf]);
 
   const handleSelectItem = useCallback((id) => {
     const alreadySelected = selectedItems.findIndex(item => item === id);
@@ -170,6 +176,8 @@ const CreatePoint: React.FC = () => {
 
       <Form onSubmit={handleSubmit} ref={formRef}>
         <h1>Cadastro do <br /> ponto de coleta</h1>
+
+        <DropZone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
